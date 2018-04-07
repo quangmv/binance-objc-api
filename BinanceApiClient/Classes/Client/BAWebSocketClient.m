a011558c16fb861983fa3612520f8201de194f60
Quang Mai Van <quang@Quangs-Air.net.fpt>
2018-04-08 00:34:12 +0700

    aaa

--- BinanceApiClient/Classes/Client/BAWebSocketClient.m
+++ BinanceApiClient/Classes/Client/BAWebSocketClient.m
@@ -40,8 +40,9 @@ typedef void (^MessageListenBlock)(NSString *message);
 }
 
 - (void)socketClientWithChannels:(NSString *)channels listenBlock:(MessageListenBlock)listenBlock {
-//    NSURLRequest *urlRequest = [[NSURLRequest alloc] initWithURL:[NSURL URLWithString:[NSString stringWithFormat:@"wss://stream.binance.com:9443/stream?streams=%@", channels]]];
-    NSURLRequest *urlRequest = [[NSURLRequest alloc] initWithURL:[NSURL URLWithString:@"wss://stream.binance.com:9443/stream?streams=ethbtc@aggTrade/ltcbtc@aggTrade/bnbbtc@aggTrade/neobtc@aggTrade/bccbtc@aggTrade/gasbtc@aggTrade/hsrbtc@aggTrade/mcobtc@aggTrade/wtcbtc@aggTrade/lrcbtc@aggTrade/qtumbtc@aggTrade/yoyobtc@aggTrade/omgbtc@aggTrade/zrxbtc@aggTrade/stratbtc@aggTrade/snglsbtc@aggTrade/bqxbtc@aggTrade/kncbtc@aggTrade/funbtc@aggTrade/snmbtc@aggTrade/iotabtc@aggTrade/linkbtc@aggTrade/xvgbtc@aggTrade/ctrbtc@aggTrade/saltbtc@aggTrade/mdabtc@aggTrade/mtlbtc@aggTrade/subbtc@aggTrade/eosbtc@aggTrade/sntbtc@aggTrade/etcbtc@aggTrade/mthbtc@aggTrade/engbtc@aggTrade/dntbtc@aggTrade/zecbtc@aggTrade/bntbtc@aggTrade/astbtc@aggTrade/dashbtc@aggTrade/oaxbtc@aggTrade/icnbtc@aggTrade/btgbtc@aggTrade/evxbtc@aggTrade/reqbtc@aggTrade/vibbtc@aggTrade/trxbtc@aggTrade/powrbtc@aggTrade/arkbtc@aggTrade/xrpbtc@aggTrade/modbtc@aggTrade/enjbtc@aggTrade/storjbtc@aggTrade/venbtc@aggTrade/kmdbtc@aggTrade/rcnbtc@aggTrade/nulsbtc@aggTrade/rdnbtc@aggTrade/xmrbtc@aggTrade/dltbtc@aggTrade/ambbtc@aggTrade/batbtc@aggTrade/bcptbtc@aggTrade/arnbtc@aggTrade/gvtbtc@aggTrade/cdtbtc@aggTrade/gxsbtc@aggTrade/poebtc@aggTrade/qspbtc@aggTrade/btsbtc@aggTrade/xzcbtc@aggTrade/lskbtc@aggTrade/tntbtc@aggTrade/fuelbtc@aggTrade/manabtc@aggTrade/bcdbtc@aggTrade/dgdbtc@aggTrade/adxbtc@aggTrade/adabtc@aggTrade/pptbtc@aggTrade/cmtbtc@aggTrade/xlmbtc@aggTrade/cndbtc@aggTrade/lendbtc@aggTrade/wabibtc@aggTrade/tnbbtc@aggTrade/wavesbtc@aggTrade/gtobtc@aggTrade/icxbtc@aggTrade/ostbtc@aggTrade/elfbtc@aggTrade/aionbtc@aggTrade/neblbtc@aggTrade/brdbtc@aggTrade/edobtc@aggTrade/wingsbtc@aggTrade/navbtc@aggTrade/lunbtc@aggTrade/trigbtc@aggTrade/appcbtc@aggTrade/vibebtc@aggTrade/rlcbtc@aggTrade/insbtc@aggTrade/pivxbtc@aggTrade/iostbtc@aggTrade/chatbtc@aggTrade/steembtc@aggTrade/nanobtc@aggTrade/viabtc@aggTrade/blzbtc@aggTrade/aebtc@aggTrade/rpxbtc@aggTrade/ncashbtc@aggTrade/poabtc@aggTrade/zilbtc@aggTrade/ontbtc@aggTrade"]];
+    NSURLRequest *urlRequest = [[NSURLRequest alloc] initWithURL:[NSURL URLWithString:[NSString stringWithFormat:@"wss://stream.binance.com:9443/stream?streams=%@", channels]]];
+    
+    NSLog(@"%@",[urlRequest.URL absoluteString]);
     
     SRWebSocket *socketClient = [[SRWebSocket alloc] initWithURLRequest:urlRequest];
     socketClient.delegate = self;
@@ -80,7 +81,7 @@ typedef void (^MessageListenBlock)(NSString *message);
 }
 
 - (void)onTradeEventWithSymbols:(NSArray *)symbols listen:(BAStreamAggTradeEventBlock)eventHandleBlock {
-    NSString *channels = [[[symbols componentsJoinedByString:@"@aggTrade/"] stringByAppendingString:@"@aggTrade"] lowercaseString];
+    NSString *channels = [[[[symbols componentsJoinedByString:@"@aggTrade/"] stringByAppendingString:@"@aggTrade"] lowercaseString] stringByReplacingOccurrencesOfString:@"aggtrade" withString:@"aggTrade"];
     [self socketClientWithChannels:channels
                       listenBlock:^(NSString *message) {
                           BAStreamAggTradeEventModel *eventModel = [[BAStreamAggTradeEventModel alloc] initWithString:message error:nil];
